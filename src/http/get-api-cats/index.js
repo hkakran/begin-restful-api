3c647539d69341b909636424ee7dba4e5728856b
Himadri Kakran <Soni@Himadris-MBP.fios-router.home>
2019-08-01 22:44:53 -0400

    return all links in get request

--- src/http/get-api-cats/index.js
+++ src/http/get-api-cats/index.js
@@ -1,7 +1,19 @@
 let data = require('@begin/data')
 
+function getRows(result) {
+  return Object.keys(result).map(function(key) {
+    return result[key];
+  }).filter((row) => {
+    return row.key;
+  });
+}
 exports.handler = async function getCats() {
-  let cats = await data.get({table:'cats'})
+  let result  = await data.get({table:'cats'})
+  let cats = getRows(result);
+  while (result.cursor) {
+    result  = await data.get({table:'cats', cursor: result.cursor})
+    cats = cats.concat(getRows(result))
+  }
   return {
     body: JSON.stringify(cats)
   }
